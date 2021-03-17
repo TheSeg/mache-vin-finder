71c18049f928b406a904fe68ac33a76f150eebd8
John "Seg" Seggerson <seg@segonmedia.com>
2021-03-16 21:17:46 -0700

    * Adding Order number support as it's now required with the API.

--- src/views/VinDetails.vue
+++ src/views/VinDetails.vue
@@ -12,6 +12,10 @@
             <input id="inputVinNumber" v-model="inputVin" placeholder="1AABB12CDEF123456" pattern="[a-hj-npr-zA-HJ-NPR-Z0-9]{17}" type="text" class="form-control" maxlength="17" required style="text-transform:uppercase;" />
           </div>
           <div class="mb-3">
+            <label for="inputOrder" class="form-label">Order Number</label>
+            <input id="inputOrder" v-model="inputOrder" placeholder="1234" type="text" class="form-control" maxlength="17" required style="text-transform:uppercase;" />
+          </div>
+          <div class="mb-3">
             <button type="submit" class="btn btn-primary">Submit</button>
           </div>
         </form>
@@ -37,6 +41,7 @@ export default {
   data () {
     return {
       inputVin: null,
+      inputOrder: null,
       output: null
     }
   },
@@ -44,17 +49,21 @@ export default {
     if (!this.inputVin && this.$route.query.vin) {
       this.inputVin = this.$route.query.vin
     }
-    if (this.inputVin) {
+    if (!this.inputOrder && this.$route.query.order) {
+      this.inputOrder = this.$route.query.order
+    }
+    if (this.inputVin && this.inputOrder) {
       this.fetchVIN()
     }
   },
   methods: {
     fetchVIN () {
-      if (this.inputVin) {
+      if (this.inputVin && this.inputOrder) {
         axios({
           url: 'https://shop.ford.com/aemservices/shop/vot/api/customerorder/',
           method: 'GET',
           params: {
+            orderNumber: this.inputOrder.toUpperCase(),
             partAttributes: 'BP2.*',
             vin: this.inputVin.toUpperCase()
           }
